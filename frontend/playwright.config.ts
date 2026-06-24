c363139275e1b0d5dc1bd6506f5f49aba41217d0
FelipeRinaldi1 <felipe.rinaldi.sobreira.dev0@gmail.com>
2026-06-23 22:46:51 -0300

    refactor: move frontend tests to tests/system to match new folder architecture

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, devices } from "@playwright/test";
 import path from "path";
 
 export default defineConfig({
-  testDir: "./integration-tests",
+  testDir: "./tests/system",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
